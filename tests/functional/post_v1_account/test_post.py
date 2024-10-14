a6e4907a2581773ac3d8f5918c434f1c90a83b0a
Алексей Федоров <trans777formator777@gmail.com>
2024-10-14 14:36:54 +0300

    Add change email method

--- tests/functional/post_v1_account/test_post.py
+++ tests/functional/post_v1_account/test_post.py
@@ -10,9 +10,15 @@ def test_post_v1_account():
     login_api= LoginApi(host= 'http://5.63.153.31:5051')
     mailhog_api= MailhogApi(host= 'http://5.63.153.31:5025')
 
-    login = 'testing_4'
+    login = 'testing_9'
     password = '6789012345'
     email = f"{login}@mail.ru"
+    login_json_data = {
+        'login': login,
+        'password': password,
+        'rememberMe': True,
+    }
+
     json_data = {
         'login': login,
         'email': email,
@@ -31,12 +37,35 @@ def test_post_v1_account():
     response = account_api.put_v1_account_token(token=token)
     assert response.status_code == 200, "User not activate"
 
+    response = login_api.post_v1_account_login(json_data=login_json_data)
+    assert response.status_code == 200, "User not authorized"
+
     json_data = {
         'login': login,
         'password': password,
-        'rememberMe': True,
+        'email' : f'{login}_changed@mail.ru'
     }
-    response = login_api.post_v1_account_login(json_data=json_data)
+    response = account_api.put_v1_account_email(json_data=json_data)
+    assert response.status_code == 200, f"Not change email for User {login}"
+
+    #  Проверка, что не можем залогиниться, если не активирован новый почтовый ящик
+    response = login_api.post_v1_account_login(json_data=login_json_data)
+    assert response.status_code == 403, "Authorize user without activate email"
+
+    # Получение последних сообщений
+    response = mailhog_api.get_api_v2_messages()
+    assert response.status_code == 200, f"Error. E-mail not found for {login}"
+
+    # Поиск по логину
+    token = get_activation_token_by_login(login, response)
+    assert token is not None, f"Not found token for {login}"
+
+    # Активация нового токена
+    response = account_api.put_v1_account_token(token=token)
+    assert response.status_code == 200, "User not activate"
+
+    # Логин
+    response = login_api.post_v1_account_login(json_data=login_json_data)
     assert response.status_code == 200, "User not authorized"
 
 
