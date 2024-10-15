d6b9e762224c6da0c4369f26922c54a5b05168e8
Алексей Федоров <trans777formator777@gmail.com>
2024-10-15 11:40:40 +0300

    Изменение структуры

---
+++ resource/resource.py
@@ -0,0 +1,15 @@
+import json
+
+base = 35
+
+def get_activation_token_by_login(
+        login,
+        response
+):
+    token = None
+    for item in response.json()['items']:
+        user_data = json.loads(item['Content']['Body'])
+        user_login = user_data['Login']
+        if user_login == login:
+            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+            return token
