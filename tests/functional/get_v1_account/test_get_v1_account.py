78a90e1b4361f1fa1b78aafc078fa084c433dda8
Алексей Федоров <trans777formator777@gmail.com>
2024-11-10 18:07:28 +0300

    Добавлены проверки хамкрестом

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,4 +1,25 @@
-def test_get_v1_account_auth(account_helper, prepare_user):
+from hamcrest import (
+    assert_that,
+    has_property,
+    starts_with,
+    all_of,
+    instance_of,
+    has_properties,
+    equal_to,
+    equal_to_ignoring_case,
+)
+from datetime import datetime
+
+from hamcrest.core.core.isnone import (
+    IsNone,
+    none,
+)
+
+
+def test_get_v1_account_auth(
+        account_helper,
+        prepare_user
+):
     login = prepare_user.login
     password = prepare_user.password
     email = prepare_user.email
@@ -7,4 +28,28 @@ def test_get_v1_account_auth(account_helper, prepare_user):
 
     account_helper.auth_client(login=login, password=password)
 
-    account_helper.dm_account_api.account_api.get_v1_account()
+    response = account_helper.dm_account_api.account_api.get_v1_account()
+
+    assert_that(
+        response, all_of(
+            has_property('resource', has_property('login', starts_with("testing_"))),
+            has_property('resource', has_property('online', instance_of(datetime))),
+            has_property(
+                'resource', has_properties(
+                    {
+                        'rating': has_properties(
+                            {
+                                "enabled": equal_to(True),
+                                "quality": equal_to(0),
+                                "quantity": equal_to(0)
+                            }
+                        ),
+                        'icq':none(),
+                        'info': ''
+
+                    }
+                )
+            )
+        )
+    )
+    print(response)
