dccd5be32bdcfbe797c6b7fd759439299244d78c
Алексей Федоров <trans777formator777@gmail.com>
2024-11-10 19:26:50 +0300

    ДЗ по контекстному менеджеру

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+import pytest
+
+from checkers.http_checkers import check_status_code_http
 from dm_api_account.models.registration import Registration
 from hamcrest import (
     assert_that,
@@ -49,4 +52,23 @@ def test_post_v1_account(
         )
     )
 
-    print(response)
+@pytest.mark.parametrize("login,password,email,error_message,expected_status_code",
+                         [
+                             ("testing2","pass", "testing2@mail.ru","Validation failed",400),
+                             ("testing3","password", "testing","Validation failed",400),
+                             ("t","password", "testing4@gmail.com","Validation failed",400)
+                         ])
+def test_post_v1_account_wit_user_params(
+        account_helper, login, password, email, error_message, expected_status_code
+):
+    login = login
+    password = password
+    email = email
+
+    registration = Registration(
+        login=login,
+        email=email,
+        password=password,
+    )
+    with check_status_code_http(expected_status_code, error_message):
+        account_helper.dm_account_api.account_api.post_v1_account(registration=registration)
